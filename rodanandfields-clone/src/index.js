f406679217d5017d5585683e1c0bb68cb172dc07
[mallikarjuna annigeri] <mallikarjunaannigeri27@gmail.com>
2022-08-26 00:20:57 +0530

    Perks program

--- rodanandfields-clone/src/index.js
+++ rodanandfields-clone/src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  
-    <ChakraProvider >
-      <App />
-    </ChakraProvider>
- 
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
