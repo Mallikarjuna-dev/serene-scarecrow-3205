d231eaaf07e34a2c787ebcf27c35e72b0e488cfe
praveen021097 <96250697+praveen021097@users.noreply.github.com>
2022-08-25 20:27:52 +0530

    second commit

--- rodanandfields-clone/src/App.js
+++ rodanandfields-clone/src/App.js
@@ -1,11 +1,17 @@
 
 
+import Footer from './Components/Footer';
+import LandingPage from './Components/LandingPage';
 import Navbar from './Components/Navbar';
+import UpperNavbar from './Components/UpperNavbar';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <UpperNavbar />
+       <Navbar /> 
+       <LandingPage />
+       <Footer />
     </div>
   );
 }
--- rodanandfields-clone/src/Components/Navbar.jsx
+++ rodanandfields-clone/src/Components/Navbar.jsx
@@ -1,139 +1,198 @@
+import { Link as routerLink } from "react-router-dom";
 import React from "react";
-import {
-  Box,
-  Link,
-  Image,
-  Text,
-  Flex,
-  Icon,
-  List,
-  ListItem,
-
-  Grid,
-  useDisclosure,
-} from "@chakra-ui/react";
-import { SearchIcon } from "@chakra-ui/icons";
-import rodanlogo from "./Image/rodan.png";
-import usaicon from "./Image/usaflag.png";
-
-import { BsPerson, BsBag } from "react-icons/bs";
-import { useRef } from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const hoverRef = useRef();
- 
   return (
-    <Box>
-      <Box
-        h={"70px"}
-        borderBottom="1px solid black"
-        display={"flex"}
-        justifyContent={"space-between"}
-      >
-        <Box w={"200px"} marginTop={"20px"} marginLeft={"30px"}>
-          <Link fontSize={"14px"}>FIND A CONSULTANT</Link>
-        </Box>
-        <Box w={"170px"} marginTop={"25px"}>
-          <Image src={rodanlogo} width={"160px"} h={"12px"}></Image>
-        </Box>
-        <Box
-          display={"flex"}
-          w={"260px"}
-          justifyContent={"space-between"}
-          marginTop={"20px"}
-          marginRight={"20px"}
-        >
-          <Box display={"flex"}>
-            <Text fontSize={"14px"}>
-              <Link>Search</Link>
-            </Text>
-            <SearchIcon marginTop={"3px"} w={"15px"} h={"15px"}></SearchIcon>
-          </Box>
-          <Box display={"flex"}>
-            <Text fontSize={"14px"}>
-              <Link>Signin</Link>
-            </Text>
-            <Icon as={BsPerson} w={"18px"} h={"18px"} marginTop={"2px"} />
-          </Box>
-          <Box>
-            <Icon as={BsBag} w={"20px"} h={"20px"}></Icon>
-          </Box>
-          <Box display={"flex"} w={"75px"} justifyContent={"space-between"}>
-            <Image
-              src={usaicon}
-              w={"18px"}
-              h={"18px"}
-              marginTop={"4px"}
-            ></Image>
-            <Text fontSize={"14px"}>
-              <Link>England</Link>
-            </Text>
-          </Box>
-        </Box>
-      </Box>
-      <Box h={"60px"} borderBottom="1px solid black">
-        <List
-          listStyleType={"none"}
-          display={"flex"}
-          justifyContent={"space-around"}
-          w={"60%"}
-          margin={"auto"}
-        >
-          <ListItem
-            marginTop={"25px"}
-            fontSize={"11px"}
-            fontWeight={"550"}
+    <div className="nav-div">
+      <ul className="ul">
+        <li className="li">
+          SHOP
+          <div className="mega-menu" id="first-mega" >
+            <div className="innerCoated">
+            <div className="inner-mega-menu" >
+              <p> SHOP BY CATEGORY</p>
+              <p>
+                <routerLink>Skincare Regimens</routerLink>
+              </p>
+              <p>
+                <routerLink>Lash & Brow</routerLink>
+              </p>
+              <p>
+                <routerLink>Eye Creams & Treatments</routerLink>
+              </p>
+              <p>
+                <routerLink>Eye Creams & Treatments</routerLink>
+              </p>
+              <p>
+                <routerLink>Eye Creams & Treatments</routerLink>
+              </p>
+              <p>
+                {" "}
+                <routerLink>Face Serums & Treatments</routerLink>
+              </p>
+              <p>
+                <routerLink>Face Masks</routerLink>
+              </p>
+              <p>
+                <routerLink>Moisturizers</routerLink>
+              </p>
+              <p>
+                {" "}
+                <routerLink>Cleansers & Makeup Removers</routerLink>
+              </p>
+              <p>
+                <routerLink>Toners</routerLink>
+              </p>
+              <p>
+                <routerLink>Sun Protection</routerLink>
+              </p>
+              <p>
+                <routerLink>Tools & Accessories</routerLink>
+              </p>
+              <p>
+                <routerLink>Hand & Body</routerLink>
+              </p>
+            </div>
+            <div className="inner-mega-menu">
+              <p> SHOP BY CONCERN</p>
+              <p>
+                <routerLink>Anti-Aging</routerLink>
+              </p>
+              <p>
+                <routerLink>Acne, Pores & Blackheads</routerLink>
+              </p>
+              <p>
+                <routerLink>Dark Spots & Uneven Skin Tone</routerLink>
+              </p>
+              <p>
+                {" "}
+                <routerLink>Sensitivity & Visible Redness</routerLink>
+              </p>
+              <p>
+                {" "}
+                <routerLink>Dull & Tired Skin</routerLink>
+              </p>
+              <p>
+                <routerLink>Dry Skin</routerLink>
+              </p>
+              <p>
+                {" "}
+                <routerLink>Teen Acne</routerLink>
+              </p>
+            </div>
+            <div className="inner-mega-menu">
+              <p>SHOP BY PRODUCT LINE</p>
+              <p>
+                {" "}
+                <routerLink>Redefine</routerLink>
+              </p>
+              <p>
+                <routerLink>Reverse</routerLink>
+              </p>
+              <p>
+                {" "}
+                <routerLink>Unblemish</routerLink>
+              </p>
+              <p>
+                <routerLink>Soothe</routerLink>
+              </p>
+              <p>
+                <routerLink>Recharge</routerLink>
+              </p>
+              <p>
+                <routerLink>Spotless</routerLink>
+              </p>
+              <p>
+                <routerLink>Enhancements</routerLink>
+              </p>
+              <p>
+                <routerLink>Pro Skincare Tools</routerLink>
+              </p>
+              <p>
+                <routerLink>Essentials</routerLink>
+              </p>
+              <p>
+                <routerLink>All Product</routerLink>
+              </p>
+            </div>
+            </div>
+          </div>
+        </li>
+        <li className="li">BEST SELLERS</li>
+        <li className="li">
+          FEATURED
+          <div className="mega-menu">
+            <div className="inner-mega-menu" id="feature-inner">
+              <p>
+                <routerLink>Special Offers</routerLink>
+              </p>
+              <p>
+                <routerLink>Before and Afters</routerLink>
+              </p>
+              <p>
+                <routerLink>New Total RF Serum</routerLink>
+              </p>
+              <p>
+                {" "}
+                <routerLink>Spotless for Teens</routerLink>
+              </p>
+              <p>
+                {" "}
+                <routerLink>Self-Care Essential</routerLink>
+              </p>
+            </div>
+          </div>
+        </li>
+        <li className="li">
+          OUR STORY
+          <div className="mega-menu">
+            <div className="inner-mega-menu">
            
-            borderBottom={"5px solid skyblue"}
-            paddingBottom={"15px"}
-          >
-            SHOP
-            <Box 
-            position={"absolute"}
-            top={"120px"}
-            left={"0px"}
-            display={"none"}
-            w={"100%"}
-            margin={"auto"}
-            h={"200px"}
-            backgroundColor={"blue"}
-            transition={"all 0.4s ease"}
-            padding={"20px"}
-            boxSizing={"border-box"}
-            justifyContent={"center"}
-            boxShadow={"0 0 5px #8888"}
-            
-            >
-                  <Box >
 
-                  </Box>
-            </Box>
-          </ListItem>
-          <ListItem marginTop={"25px"} fontSize={"11px"} fontWeight={"550"}>
-            BEST SELLERS
-          </ListItem>
-          <ListItem marginTop={"25px"} fontSize={"11px"} fontWeight={"550"} 
-           >
-            FEATURED
-          </ListItem>
-          <ListItem marginTop={"25px"} fontSize={"11px"} fontWeight={"550"} 
-            >
-            OUR STORY
-          </ListItem>
-          <ListItem marginTop={"25px"} fontSize={"11px"} fontWeight={"550"}>
-            PERKS PROGRAMS
-          </ListItem>
-          <ListItem marginTop={"25px"} fontSize={"11px"} fontWeight={"550"}>
-            BECOME A CONSULTANT
-          </ListItem>
-          <ListItem marginTop={"25px"} fontSize={"11px"} fontWeight={"550"}>
-            BLOG
-          </ListItem>
-        </List>
-      </Box>
-    
-    </Box>
+
+
+
+              <p>
+                <routerLink> R+F Difference</routerLink>
+              </p>
+              <p>
+                <routerLink>Explore Our Regimens</routerLink>
+              </p>
+              <p>
+                <routerLink>Our Founders</routerLink>
+              </p>
+              <p>
+                <routerLink>Our Impac</routerLink>
+              </p>
+              <p>
+                <routerLink>Eye Creams & Treatments</routerLink>
+              </p>
+              <p>
+                {" "}
+                <routerLink>Face Serums & Treatments</routerLink>
+              </p>
+              <p>
+                <routerLink>Face Masks</routerLink>
+              </p>
+              <p>
+                <routerLink>Moisturizers</routerLink>
+              </p>
+              <p>
+                {" "}
+                <routerLink>Cleansers & Makeup Removers</routerLink>
+              </p>
+             
+             
+            </div>
+          </div>
+        </li>
+        <li className="li">PERKS PROGRAM</li>
+        <li className="li">BECOME A CONSULTANT</li>
+        <li className="li">BLOG</li>
+      </ul>
+      <hr />
+    </div>
   );
 };
 
