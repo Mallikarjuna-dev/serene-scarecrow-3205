89210d71f13046b4c56a7cccf8feea50a9a7f5a4
[mallikarjuna annigeri] <mallikarjunaannigeri27@gmail.com>
2022-08-26 00:28:32 +0530

    updated

--- rodanandfields-clone/src/App.jsx
+++ rodanandfields-clone/src/App.jsx
@@ -1,26 +1,17 @@
-
-
-
-import Footer from './Components/Footer';
-import LandingPage from './Components/LandingPage';
-import Navbar from './Components/Navbar';
-import UpperNavbar from './Components/UpperNavbar';
+import Footer from "./Components/Footer";
+import LandingPage from "./Components/LandingPage";
+import Navbar from "./Components/Navbar";
+import UpperNavbar from "./Components/UpperNavbar";
 
 function App() {
   return (
     <div className="App">
       <UpperNavbar />
-       <Navbar /> 
-       <LandingPage />
-       <Footer />
+      <Navbar />
+      <LandingPage />
+      <Footer />
     </div>
   );
 }
 
 export default App;
-
-
-
-
-
-
