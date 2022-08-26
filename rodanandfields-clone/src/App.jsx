39f1b563f6586555449068d803c60f768a85bd3f
praveen021097 <96250697+praveen021097@users.noreply.github.com>
2022-08-26 23:02:03 +0530

    sixh

--- rodanandfields-clone/src/App.jsx
+++ rodanandfields-clone/src/App.jsx
@@ -1,19 +1,10 @@
-
-import Footer from "./Components/Footer";
-import LandingPage from "./Components/LandingPage";
-import Navbar from "./Components/Navbar";
-import UpperNavbar from "./Components/UpperNavbar";
-
 import "swiper/css/bundle";
-import "./Components/Navbar.css"
+import "./Components/Navbar.css";
 import MainRoutes from "./Pages/MainRoutes";
 function App() {
   return (
     <div className="App">
-      
-    
-     <MainRoutes />
-     
+      <MainRoutes />
     </div>
   );
 }
