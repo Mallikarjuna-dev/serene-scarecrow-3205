4d2bf48ce31fb01bea373b9fc7ac681b7eaad6fe
[mallikarjuna annigeri] <mallikarjunaannigeri27@gmail.com>
2022-08-26 21:57:21 +0530

    Update store.js

--- rodanandfields-clone/src/Redux/store.js
+++ rodanandfields-clone/src/Redux/store.js
@@ -1,15 +1,8 @@
-
-import {
-  legacy_createStore, applyMiddleware,
-
-} from "redux";
+import { legacy_createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 
 import { reducer } from "./AuthReducer/reducer";
 
-
-
 const store = legacy_createStore(reducer, applyMiddleware(thunk));
 
 export { store };
-
